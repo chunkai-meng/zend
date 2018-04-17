af2355123f71fe36d92cf3f7440d74eebccad95e
Chun Kai <CK-SmartScreen@users.noreply.github.com>
2018-04-17 18:43:58 +1200

    add album sample code

---
+++ data/album.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `album` ( `id` int(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, `artist` varchar(100) DEFAULT NULL, `title` varchar(100) DEFAULT NULL ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO album (artist, title) VALUES ('The Military Wives', 'In My Dreams');
+INSERT INTO album (artist, title) VALUES ('Adele', '21');
+INSERT INTO album (artist, title) VALUES ('Bruce Springsteen', 'Wrecking Ball (Deluxe)');
+INSERT INTO album (artist, title) VALUES ('Lana Del Rey', 'Born To Die');
+INSERT INTO album (artist, title) VALUES ('Gotye', 'Making Mirrors');
---
+++ module/Album/config/module.config.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * module.config.php
+ * Created at: 17/04/18 11:37 AM
+ */
+
+namespace Album;
+
+use Zend\Router\Http\Segment;
+
+return [
+
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AlbumController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    'view_manager' => [
+        'template_path_stack' => [
+            'album' => __DIR__ . '/../view',
+        ],
+    ],
+];
